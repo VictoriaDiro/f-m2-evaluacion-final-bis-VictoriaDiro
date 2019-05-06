97c40f6f04e7555bdad583b73f19feb291dc366f
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-06 20:16:45 +0200

    try localStorage

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,37 +1,70 @@
 'use strict';
 
-/* --> Desarrollo deinterfaz de aplicación web que simula juego de buscar parejas. --> BONUS: Implementar juego.
-
-1. MAQUETACIÓN: 
-    a. Formulario para elegir tamaño de partida y botón. X
-    b. Listado de cartas, cada elemento tiene:
-        - cara visible (pokemon)
-        - cara oculta (palabra ADALAB) --> https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB
-2. INICIO DE LA PARTIDA:
-    - Al hacer 'click' en Comenzar recoger valor de tamaño de partida y conectarse a API que devuelve listado de cartas --> https://raw.githubusercontent.com/Adalab/cards-data/master/8.json
-    - Las cartas por defecto muestran la parte trasera.
-3. ALMACENAMIENTO LOCAL:
-    - Guardar el número de cartas que la usuaria ha elegido.
-4. INTERACCIÓN:
-    - Al hacer 'click' mostraremos la parte delantera y a la inversa al volver ha hacer 'click'.
-*/
-
+// CONSTANTES GLOBALES
 const starButton = document.querySelector('.star__button');
-const input = document.querySelector('.input');
 let cards = document.querySelector('.cards');
 const backCard = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+let input = document.querySelectorAll('.input');
+console.log(input)
 
+// LOCALSTORAGE
+const saveData = value => {
+  localStorage.setItem('myInput', JSON.stringify(value));
+};
+
+const getSaveData = value => {
+  return JSON.parse(localStorage.getItem(value));
+}
+
+// const loadSaveInput = () => {
+//   const saveInput = getSaveData('myInput');
+//   saveInput.checked.innerHTML = true;
+// }
+
+// loadSaveInput();
+
+
+// JSON.parse(localStorage.getItem('myInput'));
+
+
+
+// let getInputElement = '';
+
+// function getInput() {
+//   for(let i = 0; i < input.length; i++) {
+//     if(localStorage.getItem('myInput') !== null) {
+//       getInputElement = JSON.parse(localStorage.getItem('myInput'));
+//       input[i].setAttribute('checked', 'true');
+//     } else {
+//       console.log('No hay datos el Local Storage');
+//     }
+//   }
+// }
+// getInput();
+// LOCALSTORAGE
+
+// PETICIÓN A API
 function selectNumberCards() {
 
-  fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${input.value}.json`)
+  let url = '';
+  for(let i = 0; i < input.length; i++) {
+    if(input[i].checked === true) {
+      url = `https://raw.githubusercontent.com/Adalab/cards-data/master/${input[i].value}.json`;
+
+      saveData(input[i].value);
+    }
+  }
+
+  fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data);
+      cards.innerHTML = '';
 
       for(let i = 0; i < data.length; i++) {
 
+        // CREACIÓN DE ELEMENTOS
         const cardItem = document.createElement('li');
         cardItem.classList.add('card__style');
 
@@ -48,14 +81,12 @@ function selectNumberCards() {
             image.src = backCard;
           }
         }
+
+        // CAMBIO DE CARA DE CARTA
         cardItem.addEventListener('click', changeSide);
       }
     });
 }
 
+// BÚSQUEDA DE BARAJA
 starButton.addEventListener('click', selectNumberCards);
-
-
-  // localStorage.setItem('inputValue', 4, 6, 8);
-  // const inputStorage = localStorage.getItem(inputValue);
-  // console.log(inputValue)
